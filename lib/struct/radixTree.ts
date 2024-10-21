6321aee26514a3e705c02aa4e65a8b64e79a28af
Ben Richeson <benricheson101@gmail.com>
2024-10-21 02:01:28 -0400

    make that stupid annoying npm warning go away

--- lib/struct/radixTree.ts
+++ lib/struct/radixTree.ts
@@ -1,6 +1,7 @@
 import {prefixOverlap} from '../string';
 import {Stack} from './stack';
 
+/** a node on a {@link RadixTree} */
 export class RadixNode<T> {
   edges = new Map<string, RadixNode<T>>();
   constructor(
@@ -9,6 +10,7 @@ export class RadixNode<T> {
     public isLeaf = true
   ) {}
 
+  /** depth first traversal of the RadixNode and its children */
   dft() {
     const toVisit = new Stack<RadixNode<T>>();
     toVisit.push(this);
@@ -26,6 +28,7 @@ export class RadixNode<T> {
     return results;
   }
 
+  /** depth first search of the RadixNode and its children */
   dfs(term: string, partial = false): RadixNode<T> | undefined {
     const toVisit = new Stack<RadixNode<T>>();
     toVisit.push(this);
@@ -53,9 +56,15 @@ export class RadixNode<T> {
   }
 }
 
+/**
+ * A RadixTree is a compressed Trie wherein each edge is merged with its parent
+ * to form a string. Each edge has the maximum length possible instead of a
+ * single character like a traditional trie.
+ */
 export class RadixTree<T = undefined> {
   readonly tree = new RadixNode<T>(null as T, '', false);
 
+  /** Insert a term and associated data into the tree */
   insert(
     ...args: T extends undefined ? [term: string] : [term: string, data: T]
   ) {
@@ -105,16 +114,19 @@ export class RadixTree<T = undefined> {
     return this;
   }
 
+  /** search the tree for a particular term */
   search(term: string): T | undefined {
     const bottomNode = this.tree.dfs(term);
     return bottomNode?.isLeaf ? bottomNode.data : undefined;
   }
 
+  /** test if the tree contains a particular term */
   has(term: string): boolean | undefined {
     const bottomNode = this.tree.dfs(term);
     return !!bottomNode?.isLeaf;
   }
 
+  /** get all children below a particular node. this is useful for autocompletion */
   getChildren(term: string): string[] {
     const startingNode = this.tree.dfs(term, true);
     if (!startingNode) {
