ee898f03dc3e3241e69cb5581e32ca8e500a73fe
Ben Richeson <benricheson101@gmail.com>
2024-10-20 21:45:05 -0400

    feat: radix tree getchildren completion

--- lib/struct/radixTree.ts
+++ lib/struct/radixTree.ts
@@ -1,17 +1,60 @@
 import {prefixOverlap} from '../string';
+import {Stack} from './stack';
 
 export class RadixNode<T> {
   edges = new Map<string, RadixNode<T>>();
   constructor(
     public data: T,
+    public term: string,
     public isLeaf = true
   ) {}
+
+  dft() {
+    const toVisit = new Stack<RadixNode<T>>();
+    toVisit.push(this);
+
+    const results: RadixNode<T>[] = [];
+    while (!toVisit.empty()) {
+      const node = toVisit.pop()!;
+      results.push(node);
+
+      for (const [, n] of node.edges) {
+        toVisit.push(n);
+      }
+    }
+
+    return results;
+  }
+
+  dfs(term: string): RadixNode<T> | undefined {
+    const toVisit = new Stack<RadixNode<T>>();
+    toVisit.push(this);
+
+    let _term = term;
+
+    while (!toVisit.empty()) {
+      const node = toVisit.pop()!;
+
+      if (node.term === term && node.isLeaf) {
+        return node;
+      }
+
+      const [e, n] = [...node.edges.entries()].find(([k]) => prefixOverlap(k, _term)) || [];
+
+      if (e && n) {
+        toVisit.push(n);
+        _term = _term.slice(e.length);
+      }
+    }
+  }
 }
 
-export class RadixTree<T> {
-  #tree = new RadixNode<T>(null as T, false);
+export class RadixTree<T = string> {
+  #tree = new RadixNode<T>(null as T, '', false);
 
-  insert(term: string, data: T) {
+  insert(term: string, data?: T) {
+    const _term = term;
+
     const walkTree = (tree: RadixNode<T>): RadixNode<T> => {
       for (let [edge] of tree.edges) {
         const ol = prefixOverlap(edge, term);
@@ -20,7 +63,7 @@ export class RadixTree<T> {
           if (ol !== edge) {
             const newRemEdge = edge.slice(ol.length);
 
-            const newEdge = new RadixNode(null as T, false);
+            const newEdge = new RadixNode(null as T, _term.slice(0, _term.length - term.length + ol.length), false);
             newEdge.edges.set(newRemEdge, tree.edges.get(edge)!);
 
             tree.edges.set(ol, newEdge);
@@ -43,31 +86,27 @@ export class RadixTree<T> {
     const tree = walkTree(this.#tree);
 
     if (term) {
-      tree.edges.set(term, new RadixNode(data));
+      tree.edges.set(term, new RadixNode((data || term) as T, _term));
     } else {
       tree.isLeaf = true;
-      tree.data = data;
+      tree.data = (data || term) as T;
     }
 
     return this;
   }
 
   search(term: string): T | undefined {
-    const lookupTree = (tree: RadixNode<T>, term: string): T | undefined => {
-      for (const [edge, _nde] of tree.edges) {
-        const ol = prefixOverlap(edge, term);
-        if (ol) {
-          if (ol === term) {
-            const node = tree.edges.get(term)!;
-            return node.isLeaf ? node.data : undefined;
-          }
+    const bottomNode = this.#tree.dfs(term);
+    return bottomNode?.isLeaf ? bottomNode.data : undefined;
+  }
 
-          return lookupTree(tree.edges.get(ol)!, term.slice(ol.length));
-        }
-      }
-    };
+  getChildren(term: string): string[] {
+    const startingNode = this.#tree.dfs(term);
+    if (!startingNode) {
+      return [];
+    }
 
-    return lookupTree(this.#tree, term);
+    return startingNode.dft().filter(n => n.isLeaf).map(n => n.term);
   }
 
   get tree() {
