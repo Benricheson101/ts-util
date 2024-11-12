fad5cd7819ce02a1a2840bb6e4539608425149e3
Ben Richeson <benricheson101@gmail.com>
2024-11-12 15:04:24 -0500

    test: add better test for luhn algo

--- lib/misc/luhnCheck.test.ts
+++ lib/misc/luhnCheck.test.ts
@@ -1,6 +1,11 @@
-import {strictEqual} from 'node:assert';
+import assert, {strictEqual} from 'node:assert';
 import {describe, it} from 'node:test';
-import {computeLuhnCheckDigit, isValidLuhn, toLuhn} from './luhnCheck';
+import {
+  computeLuhnCheckDigit,
+  fromLuhn,
+  isValidLuhn,
+  toLuhn,
+} from './luhnCheck';
 
 describe('misc/luhnCheck', () => {
   it('should correctly calculate the luhn check digit', () => {
@@ -14,5 +19,13 @@ describe('misc/luhnCheck', () => {
   it('should correctly verify the luhn check digit', () => {
     strictEqual(isValidLuhn(17893729974), true);
     strictEqual(isValidLuhn(17893729972), false);
+  });
+
+  it('should correctly add and verify many check digits', () => {
+    for (let i = 0; i < 100_000; i++) {
+      const signed = toLuhn(i);
+      assert(isValidLuhn(signed));
+      strictEqual(fromLuhn(signed), i);
+    }
   });
 });
