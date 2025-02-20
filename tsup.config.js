cdb4ae11ccf41259d2d38106f5be02eb5d0a24f4
Ben Richeson <benricheson101@gmail.com>
2025-02-20 18:23:29 -0500

    use tsc for building for tests because node coverage doesnt like tsup

--- tsup.config.js
+++ tsup.config.js
@@ -1,11 +1,12 @@
 import {defineConfig} from 'tsup';
 
 export default defineConfig({
-  entry: ['./lib/**/*.ts'],
+  entry: ['./lib/**/*.ts', '!./lib/**/*.test.ts'],
   outDir: './build',
   dts: true,
   treeshake: true,
   tsconfig: './tsconfig.json',
   format: ['esm', 'cjs'],
   clean: true,
+  keepNames: true,
 });
